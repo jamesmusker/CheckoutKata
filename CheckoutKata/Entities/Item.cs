09ec31a5209084b3b295104bae0f324fc6d3e108
James Musker <jimusker@gmail.com>
2022-01-10 16:35:46 +0000

    Added the Item base class

---
+++ CheckoutKata/Entities/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace CheckoutKata.Entities
+{
+    public class Item
+    {
+        public Item(string sku, decimal unitPrice)
+        {
+            SKU = sku;
+            UnitPrice = unitPrice;
+        }
+
+        public string SKU { get; private set; }
+        public decimal UnitPrice { get; private set; }
+    }
+}
