2ad32db0f44ced4f855384b8fbf05c10a82c20dd
James Musker <jimusker@gmail.com>
2022-01-10 16:57:23 +0000

    Added the Special Offer base class

---
+++ CheckoutKata/SpecialOffer.cs
@@ -0,0 +1,16 @@
+﻿namespace CheckoutKata
+{
+    public class SpecialOffer
+    {
+        public SpecialOffer(string sku, int quantity, decimal unitPrice)
+        {
+            SKU = sku;
+            Quantity = quantity;
+            UnitPrice = unitPrice;
+        }
+
+        public string SKU { get; private set; }
+        public int Quantity { get; private set; }
+        public decimal UnitPrice { get; private set; }
+    }
+}
