3a758c08af287413c74471fb58bf2f3e9e320bfb
James Musker <jimusker@gmail.com>
2022-01-10 16:52:30 +0000

    Added the tests for Checkout - Total

--- Tests/CheckoutKata.Tests.Unit/Entities/CheckoutTests.cs
+++ Tests/CheckoutKata.Tests.Unit/Entities/CheckoutTests.cs
@@ -33,5 +33,52 @@ namespace CheckoutKata.Tests.Unit.Entities
                 .Should()
                 .Be(2);
         }
+
+        [Fact]
+        public void TotalIsCorrectWithNoItems()
+        {
+            var checkout = new Checkout();
+            decimal total = checkout.Total();
+
+            total.Should().Be(0M);
+        }
+
+        [Fact]
+        public void TotalIsCorrectWithSingleItem()
+        {
+            var checkout = new Checkout();
+            var item = new Item("A99", 0.5M);
+
+            checkout.Scan(item);
+            decimal total = checkout.Total();
+            total.Should().Be(0.5M);
+        }
+
+        [Fact]
+        public void TotalIsCorrectWithMultipleItems()
+        {
+            var checkout = new Checkout();
+            var item1 = new Item("A99", 0.5M);
+            var item2 = new Item("B15", 0.3M);
+
+            checkout.Scan(item1);
+            checkout.Scan(item2);
+
+            decimal total = checkout.Total();
+            total.Should().Be(0.8M);
+        }
+
+        [Fact]
+        public void TotalIsCorrectWithMultipleOfSingleItem()
+        {
+            var checkout = new Checkout();
+            var item = new Item("A99", 0.5M);
+
+            checkout.Scan(item);
+            checkout.Scan(item);
+
+            decimal total = checkout.Total();
+            total.Should().Be(1M);
+        }
     }
 }
