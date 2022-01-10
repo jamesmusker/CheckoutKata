ff05e50ebdf8b0d51d7dc1b8fd8a733f444fd74f
James Musker <jimusker@gmail.com>
2022-01-10 16:42:06 +0000

    Added the test for Checkout Scan

---
+++ CheckoutKata/Entities/Checkout.cs
@@ -0,0 +1,19 @@
+﻿namespace CheckoutKata.Entities
+{
+    public class Checkout
+    {
+        private List<Item> _items = new List<Item>();
+
+        public IEnumerable<Item> Items => _items.AsReadOnly();
+
+        public decimal Total()
+        {
+            return 0M;
+        }
+
+        public void Scan(Item item)
+        {
+
+        }
+    }
+}
---
+++ Tests/CheckoutKata.Tests.Unit/Entities/CheckoutTests.cs
@@ -0,0 +1,22 @@
+﻿using CheckoutKata.Entities;
+using FluentAssertions;
+using System.Linq;
+using Xunit;
+
+namespace CheckoutKata.Tests.Unit.Entities
+{
+    public class CheckoutTests
+    {
+        [Fact]
+        public void CanAddItem()
+        {
+            var checkout = new Checkout();
+            var item = new Item("A99", 0.5M);
+
+            checkout.Scan(item);
+            checkout.Items.Count()
+                .Should()
+                .Be(1);
+        }
+    }
+}
